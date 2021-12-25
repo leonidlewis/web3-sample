37eaab7f582d0f574a2def852337879ceb6afacc
leonidlewis <leonid.lewis@gmail.com>
2021-12-25 22:44:25 +0800

    next + material ui

--- pages/index.js
+++ pages/index.js
@@ -1,56 +1,25 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <nav>
+          <Link href="/"><a className="nav">Home</a></Link>
+          <Link href="/challenge"><a className="nav">Challenge</a></Link>
+          <Link href="/reward"><a className="nav">Reward</a></Link>
+          <Link href="/claim"><a className="nav">Claim</a></Link>
+        </nav>
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+       
       </main>
 
       <footer>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -60,6 +29,9 @@ export default function Home() {
       </footer>
 
       <style jsx>{`
+        .nav {
+          padding: 20px;
+        }
         .container {
           min-height: 100vh;
           padding: 0 0.5rem;
